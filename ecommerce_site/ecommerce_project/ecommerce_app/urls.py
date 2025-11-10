fb800641e9875fccff34ff4eb00e9f23ac7ea334
M-Swathi22 <swathi.email1@gmail.com>
2025-11-10 12:13:46 +0530

    Added dummy URLs and placeholder views for Categories, and Search

--- ecommerce_site/ecommerce_project/ecommerce_app/urls.py
+++ ecommerce_site/ecommerce_project/ecommerce_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+     path('', views.home, name='home'),
      path('register/', views.register, name='register'),
      path('login/', views.login_view, name='login'),
+     path('search/', views.search, name='search'),
+     path('categories/',views.categories,name='categories'),
+     
 ]
